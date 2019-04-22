596e06052c18bacda43ee8fd3cbda7a082777fae
HexTree <lamencel@gmail.com>
2019-04-22 17:02:53 +0100

    move loading of items out of ready routine

--- uncorrupted_turkey/uncorrupted_turkey.py
+++ uncorrupted_turkey/uncorrupted_turkey.py
@@ -8,7 +8,7 @@ from common.items import load_items, get_item
 
 BOT_PREFIX = ('!', '?')
 TOKEN = 'NTY5MTY3NzAyNDM4OTY5Mzg0.XLsvXQ.riryPznr8KknKGWv9CG8lNg2syQ'
-items = {}
+ITEMS = load_items()
 
 client = Bot(command_prefix=BOT_PREFIX, status=Status.online, activity=Game("New World"))
 
@@ -16,8 +16,6 @@ client = Bot(command_prefix=BOT_PREFIX, status=Status.online, activity=Game("New
 # EVENTS
 @client.event
 async def on_ready():
-    global items
-    items = load_items()
     print("Logged in as " + client.user.name)
 
 
@@ -80,7 +78,6 @@ async def hello(ctx):
                 brief='Describe the queried item')
 async def item(ctx, *args):
     THRESH = 30  # if query is more than THRESH from any known items, it's probably not one we know
-    global items
     message = []
     item_name = ' '.join(args).lower().strip()
     print("Querying item: " + item_name)
@@ -89,8 +86,8 @@ async def item(ctx, *args):
     if item_name == 'turkey':
         await ctx.send(turkey_message(":angry: A turkey is not an item, we are the original inhabitants of this island."))
         return
-    if item_name not in items:
-        best_guess, distance = get_item(item_name, items)
+    if item_name not in ITEMS:
+        best_guess, distance = get_item(item_name, ITEMS)
         print("'{}' '{}' distance: {}".format(item_name, best_guess, distance))
         if distance > THRESH:
             await ctx.send(turkey_message("I don't know the item: {}. Try to write the name as you see it in game (not case sensitive). Note that I only know about items in the Refining and Blacksmithing categories right now.".format(italics(item_name))))
@@ -99,7 +96,7 @@ async def item(ctx, *args):
             message.append(turkey_message("I don't know the item: {}. I'm going to go out on a turkey leg and guess you meant the item: {}?".format(italics(item_name), italics(best_guess))))
             item_name = best_guess
 
-    item = items[item_name]
+    item = ITEMS[item_name]
     message.append(bold('Item') + ': ' + italics(item_name))
 
     # tier
