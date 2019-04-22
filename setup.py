e91cb0c4fca1cb253ad7597e8c5930d507e520f0
HexTree <lamencel@gmail.com>
2019-04-22 17:30:21 +0100

    make common a package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Liam Mencel',
     version='0.1dev',
     description='Discord bots related to the game: New World',
-    packages=find_packages(exclude=('tests', 'docs', 'data', 'common')),
+    packages=find_packages(exclude=('tests', 'docs', 'data')),
     license='MIT',
     long_description=open('README.md').read(),
     python_requires='>=3.6.0',
